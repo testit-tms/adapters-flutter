096bd106840b896704ad6905051c926404b67bd3
Petr Komissarov <petr.komissarov@testit.software>
2024-06-25 16:29:34 +0300

    Add validation_service.dart

--- lib/managers/config_manager.dart
+++ lib/managers/config_manager.dart
@@ -3,12 +3,11 @@ import 'dart:io';
 import 'package:adapters_flutter/models/config/env_config_model.dart';
 import 'package:adapters_flutter/models/config/file_config_model.dart';
 import 'package:adapters_flutter/models/config/merged_config_model.dart';
-import 'package:adapters_flutter/models/exception_model.dart';
 import 'package:adapters_flutter/services/config/env_config_service.dart';
 import 'package:adapters_flutter/services/config/file_config_service.dart';
+import 'package:adapters_flutter/services/validation_service.dart';
 import 'package:path/path.dart' as path;
 import 'package:synchronized/synchronized.dart';
-import 'package:uuid/uuid.dart';
 
 MergedConfigModel? _config;
 final _lock = Lock();
@@ -21,7 +20,7 @@ Future<MergedConfigModel> getConfigAsync() async {
       final envConfig = await getConfigFromEnvAsync();
 
       _config = _mergeConfigs(fileConfig, envConfig);
-      _validateConfig(_config);
+      await validateConfigAsync(_config);
     }
   });
 
@@ -93,44 +92,6 @@ MergedConfigModel _mergeConfigs(
   }
 
   return config;
-}
-
-void _validateConfig(final MergedConfigModel? config) async {
-  if (config == null) {
-    throw const TmsConfigException('Config is null');
-  }
-
-  if (config.adapterMode == 0 || config.adapterMode == 1) {
-    if (config.testRunId == null ||
-        !Uuid.isValidUUID(fromString: config.testRunId!)) {
-      throw const TmsConfigException('TestRunID is invalid');
-    }
-  } else if (config.adapterMode == 2) {
-    if (config.testRunId != null && config.testRunId!.isNotEmpty) {
-      throw const TmsConfigException(
-          'TestRunID should be absent in adapter mode 2');
-    }
-  } else {
-    throw TmsConfigException('Invalid adapter mode: ${config.adapterMode}');
-  }
-
-  if (config.projectId == null ||
-      !Uuid.isValidUUID(fromString: config.projectId!)) {
-    throw const TmsConfigException('ProjectId is invalid');
-  }
-
-  if (config.configurationId == null ||
-      !Uuid.isValidUUID(fromString: config.configurationId!)) {
-    throw const TmsConfigException('ConfigurationId is invalid');
-  }
-
-  if (config.privateToken == null || config.privateToken!.isEmpty) {
-    throw const TmsConfigException('PrivateToken is invalid');
-  }
-
-  if (config.url == null || !Uri.parse(config.url!).isAbsolute) {
-    throw const TmsConfigException('Url is invalid');
-  }
 }
 
 MergedConfigModel _updateUrl(final MergedConfigModel config) {
