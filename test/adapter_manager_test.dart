d3b407f7c264e48e9f7b944ab09bf66f56d8ddcc
Petr Komissarov <petr.komissarov@testit.software>
2024-07-01 19:28:43 +0300

    Add unit tests

---
+++ test/adapter_manager_test.dart
@@ -0,0 +1,26 @@
+import 'package:adapters_flutter/adapters_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('adapter manager', () {
+    test('add attachment', () {
+      expect(() async => await addAttachment(''), returnsNormally);
+    });
+
+    test('add attachments', () {
+      expect(() async => await addAttachments(['']), returnsNormally);
+    });
+
+    test('add link', () {
+      expect(() async => await addLink(''), returnsNormally);
+    });
+
+    test('add links', () {
+      expect(() async => await addLinks([Link('')]), returnsNormally);
+    });
+
+    test('add message', () {
+      expect(() async => await addMessage(''), returnsNormally);
+    });
+  });
+}
---
+++ test/link_test.dart
@@ -0,0 +1,11 @@
+import 'package:adapters_flutter/adapters_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('link', () {
+    test('create', () {
+      expect(() => Link('', description: '', title: '', type: LinkType.issue),
+          returnsNormally);
+    });
+  });
+}
