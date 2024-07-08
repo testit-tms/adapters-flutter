5ae27399d41b52bf5e135aeec7eec06a39ec85b5
Petr Komissarov <petr.komissarov@testit.software>
2024-07-08 11:26:49 +0300

    Add 'flutter_test' export in 'adapters_flutter' package.

--- lib/adapters_flutter.dart
+++ lib/adapters_flutter.dart
@@ -5,3 +5,4 @@ export 'package:adapters_flutter/src/managers/adapter_manager.dart';
 export 'package:adapters_flutter/src/managers/step_manager.dart';
 export 'package:adapters_flutter/src/managers/test_manager.dart';
 export 'package:adapters_flutter/src/models/api/link_api_model.dart' show Link;
+export 'package:flutter_test/flutter_test.dart';
--- test/test_manager_test.dart
+++ test/test_manager_test.dart
@@ -1,7 +1,6 @@
 #!/usr/bin/env dart
 
 import 'package:adapters_flutter/adapters_flutter.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('test manager', () {
