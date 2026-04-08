d9520c8f776d8c1cf364ae0ff4b805179e00e2db
pavel.butuzov <pavel.butuzov@testit.software>
2026-04-08 15:49:27 +0300

    fix models

--- lib/src/service/api/work_item_api_service.dart
+++ lib/src/service/api/work_item_api_service.dart
@@ -4,7 +4,7 @@ import 'package:testit_adapter_flutter/src/model/config_model.dart';
 import 'package:testit_adapter_flutter/src/service/api/api_client_factory.dart';
 import 'package:testit_api_client_dart/api.dart';
 
-Future<WorkItemModel?> getWorkItemById(
+Future<WorkItemApiResult?> getWorkItemById(
     final ConfigModel config, final String? workItemId) async {
   final workItemsApi = createApiClient<WorkItemsApi>(config);
   return workItemsApi.getWorkItemById(workItemId!);
